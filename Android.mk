8ba3f48d63cf8219f3be1cc2a91ea2246e324aba
Samuel Holland <mevordel@gmail.com>
2012-11-20 19:45:36 -0600

    Prepare for possible future support of venturi_kor and venturi_intl
    Change-Id: I50d59b68c3572359b5d5c3c048b9be4af5f92fa7

--- Android.mk
+++ Android.mk
@@ -16,9 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),venturi)
 
-ifneq ($(TARGET_SIMULATOR),true)
 include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
 
 endif
 
--- extract-files.sh
+++ extract-files.sh
@@ -15,9 +15,10 @@
 # limitations under the License.
 
 DEVICE=venturi
+MANUFACTURER=samsung
 
-rm -rf ../../../vendor/samsung/$DEVICE/*
-mkdir -p ../../../vendor/samsung/$DEVICE/proprietary
+rm -rf ../../../vendor/$MANUFACTURER/$DEVICE/*
+mkdir -p ../../../vendor/$MANUFACTURER/$DEVICE/proprietary
 
 if [ -f "$1" ]; then
 	rm -rf tmp
@@ -36,10 +37,13 @@ fi
 DIRS="
 bin
 cameradata
+etc/audio
 etc/wifi
+firmware
 lib/egl
 media
-firmware
+usr/share/alsa/cards
+usr/share/alsa/pcm
 vendor/bin
 vendor/firmware
 vendor/etc
@@ -49,7 +53,7 @@ vendor/lib/hw
 "
 
 for DIR in $DIRS; do
-	mkdir -p ../../../vendor/samsung/$DEVICE/proprietary/$DIR
+	mkdir -p ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/$DIR
 done
 
 FILES="
@@ -208,14 +212,14 @@ etc/audio/Tx_ControlParams_WIRED_HEADSET_WIDEBAND.txt
 
 for FILE in $FILES; do
 	if [ "$ZIP" ]; then
-		cp tmp/system/"$FILE" ../../../vendor/samsung/$DEVICE/proprietary/$FILE
+		cp tmp/system/"$FILE" ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/$FILE
 	else
-		adb pull system/$FILE ../../../vendor/samsung/$DEVICE/proprietary/$FILE
+		adb pull system/$FILE ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/$FILE
 	fi
 done
 if [ "$ZIP" ]; then rm -rf tmp ; fi
 
-(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/samsung/$DEVICE/$DEVICE-vendor-blobs.mk
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/$MANUFACTURER/$DEVICE/$DEVICE-vendor-blobs.mk
 # Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -436,5 +440,57 @@ PRODUCT_COPY_FILES += \\
 
 EOF
 
-./setup-makefiles.sh
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__MANUFACTURER__/$MANUFACTURER/g > ../../../vendor/$MANUFACTURER/$DEVICE/$DEVICE-vendor.mk
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# Live wallpaper packages and Themes
+PRODUCT_PACKAGES := \\
+    LiveWallpapers \\
+    LiveWallpapersPicker \\
+    MagicSmokeWallpapers \\
+    VisualizationWallpapers \\
+    librs_jni
+
+# Publish that we support the live wallpaper feature.
+PRODUCT_COPY_FILES := \\
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+\$(call inherit-product, vendor/$MANUFACTURER/$DEVICE/$DEVICE-vendor-blobs.mk)
+EOF
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__MANUFACTURER__/$MANUFACTURER/g > ../../../vendor/$MANUFACTURER/$DEVICE/BoardConfigVendor.mk
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+USE_CAMERA_STUB := false
+
+BOARD_USES_GENERIC_AUDIO := false
+
+BOARD_USES_LIBSECRIL_STUB := false
+
+BOARD_NO_PAGE_FLIPPING := false
+BOARD_NO_32BPP := false
+EOF
