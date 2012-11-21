c10f326aaca776e0e02dd2961602e0f180ac0a46
Samuel Holland <mevordel@gmail.com>
2012-11-21 12:12:18 -0600

    Fix integrated kernel/ramdisk; cooperate with cm build system
    Change-Id: I9c01330048f8c4d3bf3dfd65a1d9562754d1dd9e

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
+# Add ramdisk dependencies to kernel
+TARGET_KERNEL_BINARIES: $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET)
+
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Boot image: $@")
