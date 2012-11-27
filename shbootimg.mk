99fc80aede790459fe4eddb5d19078a399325fb6
Samuel Holland <mevordel@gmail.com>
2012-11-26 21:07:44 -0600

    Look for script in correct directory
    Change-Id: I7e611ad0d74a9ebbe4ca4f138fcec9a27924717e

--- shbootimg.mk
+++ shbootimg.mk
@@ -5,7 +5,7 @@ TARGET_KERNEL_BINARIES: $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET)
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Boot image: $@")
-	$(hide) device/samsung/venturi_usa/mkshbootimg.py $@ $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+	$(hide) device/samsung/venturi/mkshbootimg.py $@ $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET) | $(ACP)
 	$(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
