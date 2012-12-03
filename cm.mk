698bea7ecc5b98562cd4dde9e132cb5afab6684c
Samuel Holland <mevordel@gmail.com>
2012-12-03 13:57:22 -0600

    We are not a phone / do not have SMS
    Change-Id: I852b4682da6506d86962dd01046da20399216794

--- cm.mk
+++ cm.mk
@@ -1,10 +1,7 @@
-# Specify phone tech first.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 PRODUCT_RELEASE_NAME := GalaxyPlayer
 # Inherit some common CM stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
 
 # Inherit device configuration.
 $(call inherit-product, device/samsung/venturi/full_venturi.mk)
