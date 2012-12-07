bceb108237c9c56c9be1380f8fc54ce42aadf936
Samuel Holland <mevordel@gmail.com>
2012-12-06 20:04:53 -0600

    boot: Fix dependency and variable typos
    Change-Id: I38f2ae32f968745e34d2fde1c3c88cd1cf1d6c03

--- bootimg.mk
+++ bootimg.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) | $(ACP)
+$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET)
 	$(call pretty,"Boot image: $@")
-	$(hide) ./device/samsung/venturi/mkbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+	$(hide) ./device/samsung/venturi/mkbootimg.py $@ $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
 
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET) | $(ACP)
 	$(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
