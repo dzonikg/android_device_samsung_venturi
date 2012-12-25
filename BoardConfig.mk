079bc7e17818a9c1dc4554625a7040787e47e79e
Samuel Holland <mevordel@gmail.com>
2012-12-25 12:46:33 -0600

    Update/Fix/Reorganize BoardConfig.mk
    Change-Id: Ife37a0f2dc089f703f40a4b9f51f202f2f348b24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,13 +25,13 @@ BOARD_USES_LIBSECRIL_STUB := true
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/venturi/BoardConfigVendor.mk
 
+# Platform
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
-ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
@@ -43,35 +43,38 @@ TARGET_BOOTLOADER_BOARD_NAME := s5pc110
 # Releasetools
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/venturi/releasetools/venturi_ota_from_target_files
 
-# Camera
-ifeq ($(USE_CAMERA_STUB),false)
-BOARD_CAMERA_LIBRARIES := libcamera
-endif
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-
-# Video Devices
-BOARD_USES_OVERLAY := true
-BOARD_V4L2_DEVICE := /dev/video1
-BOARD_CAMERA_DEVICE := /dev/video0
-BOARD_SECOND_CAMERA_DEVICE := /dev/video2
-
-# Device related defines
+# Kernel
+TARGET_KERNEL_CONFIG := yp_g70_usa_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/venturi/bootimg.mk
 BOARD_KERNEL_BASE := 0x32000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_CMDLINE := console=ttyFIQ0,115200 init=/init no_console_suspend
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/venturi/bootimg.mk
-TARGET_KERNEL_CONFIG := yp_g70_usa_defconfig
 TARGET_PROVIDES_INIT_RC := true
-TARGET_USERIMAGES_USE_EXT4 := true
+
+# Recovery
+TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /cache/.startrecovery; sync;"
+TARGET_RECOVERY_INITRC := device/samsung/venturi/recovery.rc
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/venturi/recovery/graphics.c
+BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_OTA_ASSERT_DEVICE := venturi,YP-G70,venturi_usa
 
+# Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 47864320
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 304087040
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2017460224
 BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 17
+#BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+
+# USB
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun%d/file
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+SAMSUNG_USB_MTP_DEVICE := true
 
 # Connectivity - Wi-Fi
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
@@ -86,21 +89,20 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 17
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
 
-# Recovery
-TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /cache/.startrecovery; sync;"
-TARGET_RECOVERY_INITRC := device/samsung/venturi/recovery.rc
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/venturi/recovery/graphics.c
-BOARD_HAS_NO_SELECT_BUTTON := true
+# Camera
+ifeq ($(USE_CAMERA_STUB),false)
+BOARD_CAMERA_LIBRARIES := libcamera
+endif
 
-# USB
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun%d/file
-BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
-SAMSUNG_USB_MTP_DEVICE := true
+# Video Devices
+BOARD_USES_OVERLAY := true
+BOARD_V4L2_DEVICE := /dev/video1
+BOARD_CAMERA_DEVICE := /dev/video0
+BOARD_SECOND_CAMERA_DEVICE := /dev/video2
 
 # SkTextBox for libtvout
 BOARD_USES_SKTEXTBOX := true
@@ -110,23 +112,20 @@ USE_OPENGL_RENDERER := true
 BOARD_ALLOW_EGL_HIBERNATION := true
 BOARD_EGL_CFG := device/samsung/venturi/egl.cfg
 
+# Touchscreen
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
 # FM Radio
 BOARD_HAVE_FM_RADIO := true
 BOARD_FM_DEVICE := si4709
 
-# Touchscreen
-BOARD_USE_LEGACY_TOUCHSCREEN := true
-
 # Audio
 BOARD_USES_ALSA_AUDIO := true
-BOARD_USES_AUDIO_LEGACY := true
+#BOARD_USES_AUDIO_LEGACY := true
 TARGET_PROVIDES_LIBAUDIO := true
-
 
 
 
 # hwcomposer: custom vsync ioctl
 #BOARD_CUSTOM_VSYNC_IOCTL := true
-#TARGET_PROVIDES_LIBAUDIO := true
-#COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_EXTERNAL_IMAGE -DMISSING_EGL_PIXEL_FORMAT_YV12 -DMISSING_GRALLOC_BUFFERS
 #BOARD_USE_SKIA_LCDTEXT := true
