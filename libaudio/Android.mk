7386d1643505f82fb3e56d4c7d010444b587e500
Samuel Holland <mevordel@gmail.com>
2013-01-01 17:28:25 -0600

    audio: Remove mc1n2 OSS HAL and replace with one from Samsung p1
    Change-Id: I515e06000e1e41b962c11b86656dbba99f963e15

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -1,53 +1,41 @@
-# Copyright (C) 2012 Paul Kocialkowski <contact@paulk.fr>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+LOCAL_PATH:= $(call my-dir)
 
-LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= \
+	AudioHardware.cpp
+
+LOCAL_MODULE := audio.primary.s5pc110
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_STATIC_LIBRARIES:= libmedia_helper
+LOCAL_SHARED_LIBRARIES:= \
+	libutils \
+	libhardware_legacy \
+	libtinyalsa \
+	libaudioutils
+
+LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES += libdl
+LOCAL_C_INCLUDES += \
+	external/tinyalsa/include \
+	system/media/audio_effects/include \
+	system/media/audio_utils/include
+
+include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-	audio_hw.c \
-	audio_out.c \
-	audio_in.c \
-	audio_ril_interface.c \
-	mixer.c
-
-LOCAL_C_INCLUDES += \
-	external/tinyalsa/include \
-	external/expat/lib \
-	system/media/audio_utils/include \
-	system/media/audio_effects/include \
-	hardware/tinyalsa-audio/include \
-	$(LOCAL_PATH)/../yamaha-mc1n2-audio/include
-
-LOCAL_SHARED_LIBRARIES := \
-	libc \
-	libcutils \
-	libutils \
-	libexpat \
-	libtinyalsa \
-	libaudioutils \
-	libdl \
-	libyamaha-mc1n2-audio
-
-LOCAL_CFLAGS += -DYAMAHA_MC1N2_AUDIO -DYAMAHA_MC1N2_AUDIO_DEVICE=\"venturi\"
-
-LOCAL_PRELINK_MODULE := false
+LOCAL_SRC_FILES := AudioPolicyManager.cpp
+LOCAL_SHARED_LIBRARIES := libcutils libutils libmedia
+LOCAL_STATIC_LIBRARIES := libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
+LOCAL_MODULE := audio_policy.s5pc110
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+  LOCAL_CFLAGS += -DWITH_A2DP
+endif
 
 include $(BUILD_SHARED_LIBRARY)
