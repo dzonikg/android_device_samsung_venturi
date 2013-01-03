0a0576e29050d88b70a2bf6db4f49578ba92514a
Samuel Holland <mevordel@gmail.com>
2013-01-02 20:38:37 -0600

    camera: Use crespo library for better compatibility
    Change-Id: Ied13f89268c5e5090c9c65c0e81d60b67c6aa7b2

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -5,7 +5,6 @@ include $(CLEAR_VARS)
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.product.board>.so
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
 LOCAL_C_INCLUDES += hardware/samsung/exynos3/s5pc110/include
 LOCAL_C_INCLUDES += hardware/samsung/exynos3/s5pc110/libs3cjpeg
 LOCAL_C_INCLUDES += frameworks/native/include/media/hardware
@@ -15,12 +14,11 @@ LOCAL_SRC_FILES:= \
 	SecCameraHWInterface.cpp \
 	SecCameraUtils.cpp \
 
-LOCAL_SHARED_LIBRARIES:= libutils libcutils libbinder liblog libcamera_client libhardware
-LOCAL_SHARED_LIBRARIES+= libs3cjpeg
+LOCAL_SHARED_LIBRARIES := libutils libcutils libbinder liblog libcamera_client libhardware
+LOCAL_SHARED_LIBRARIES += libs3cjpeg
 
 LOCAL_MODULE := camera.s5pc110
 
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
